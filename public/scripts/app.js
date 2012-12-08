106d1cebd7fc3ee043f07e26b5b86ae49ed8143c
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2012-12-08 14:13:16 -0500

    Change to full MVC and router ember implementation.
    As preparation for adding additional pages, let go ahead and switch to
    the full implementation for ember routing and acutally using models and
    views in addition to the templates and controllers.  Last but not least,
    throw in a router, and adding new pages should be a breeze.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,39 @@
 (function(win) {
-  var app = Ember.Application.create({rootElement: '#app'});
+  var app = Ember.Application.create();
 
-  app.productsController = Ember.ArrayController.create({
-    content: [],
-  }),
+  app.Product = Ember.Object.extend();
+  app.Product.reopenClass({
+    _listOfProducts: Ember.A(),
+    all: function() {
+      var allProducts = this._listOfProducts;
 
-  $.getJSON('/api/products', function(products) {
-    app.productsController.setObjects(products);
+      $.getJSON('/api/products', function(products) {
+        allProducts.setObjects(products);
+      });
+
+      return this._listOfProducts;
+    }
+  });
+
+  app.ApplicationController = Ember.Controller.extend();
+  app.ProductsController = Ember.ArrayController.extend(),
+
+  app.ApplicationView = Ember.View.extend({templateName: 'application'});
+  app.ComingSoonView = Ember.View.extend({templateName: 'comingSoon'});
+  app.CopyrightView = Ember.View.extend({templateName: 'copyright'});
+  app.ProductsView = Ember.View.extend({templateName: 'products'});
+
+  app.Router = Ember.Router.extend({
+    root: Ember.Route.extend({
+      index: Ember.Route.extend({
+        route: '/',
+        connectOutlets: function(router, context) {
+          router.get('applicationController').connectOutlet('header', 'comingSoon');
+          router.get('applicationController').connectOutlet('body', 'products', app.Product.all());
+          router.get('applicationController').connectOutlet('footer', 'copyright');
+        }
+      })
+    })
   });
 
   win.NKK = app;
